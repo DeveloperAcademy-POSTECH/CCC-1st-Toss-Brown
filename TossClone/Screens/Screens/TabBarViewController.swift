388407ba7df381795e11366e34b19b16f38b0b9b
JI SOO <dlwltn815@gmail.com>
2022-06-28 23:11:20 +0900

    Feat: Tab Bar UI 컴포넌트 생성 (#3)

--- TossClone/Screens/Screens/TabBarViewController.swift
+++ TossClone/Screens/Screens/TabBarViewController.swift
@@ -8,8 +8,49 @@
 import UIKit
 
 class TabBarController: UITabBarController {
+    private lazy var homeViewController: UIViewController = {
+        let viewController = UIViewController()
+        let tabBarItem = UITabBarItem(title: "홈", image: UIImage(systemName: "house.fill"), tag: 0)
+        viewController.tabBarItem = tabBarItem
+        
+        return viewController
+    }()
+    
+    private lazy var bonusViewController: UIViewController = {
+        let viewController = UIViewController()
+        let tabBarItem = UITabBarItem(title: "혜택", image: UIImage(systemName: "diamond.fill"), tag: 0)
+        viewController.tabBarItem = tabBarItem
+        
+        return viewController
+    }()
+    
+    private lazy var sendMondeyViewController: UIViewController = {
+        let viewController = UIViewController()
+        let tabBarItem = UITabBarItem(title: "송금", image: UIImage(systemName: "dollarsign.circle.fill"), tag: 0)
+        viewController.tabBarItem = tabBarItem
+        
+        return viewController
+    }()
+    
+    private lazy var stockViewController: UIViewController = {
+        let viewController = UIViewController()
+        let tabBarItem = UITabBarItem(title: "주식", image: UIImage(systemName: "chart.bar.fill"), tag: 0)
+        viewController.tabBarItem = tabBarItem
+        
+        return viewController
+    }()
+    
+    private lazy var allViewController: UIViewController = {
+        let viewController = UIViewController()
+        let tabBarItem = UITabBarItem(title: "전체", image: UIImage(systemName: "line.3.horizontal"), tag: 0)
+        viewController.tabBarItem = tabBarItem
+        
+        return viewController
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        viewControllers = [homeViewController, bonusViewController, sendMondeyViewController, stockViewController, allViewController]
     }
 }
