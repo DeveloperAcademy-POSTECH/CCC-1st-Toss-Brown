da544910ce9a447e5344f8af3d40f2d4ec67465b
JI SOO <dlwltn815@gmail.com>
2022-06-28 20:09:08 +0900

    Chore: 프로젝트 생성 및 main 스토리보드 삭제

---
+++ TossClone/SceneDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  SceneDelegate.swift
+//  TossClone
+//
+//  Created by 이지수 on 2022/06/28.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.backgroundColor = .systemBackground
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
+
