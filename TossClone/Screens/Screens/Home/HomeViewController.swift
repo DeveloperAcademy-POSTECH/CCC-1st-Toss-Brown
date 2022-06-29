92a904f2c592598c94a8318c2313d68570ec2964
JI SOO <dlwltn815@gmail.com>
2022-06-29 19:35:24 +0900

    Feat: 임시로 home 뷰 만들어서 탭 바뀌는지 확인 (#3)
    - HomeViewController 생성

---
+++ TossClone/Screens/Screens/Home/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  TossClone
+//
+//  Created by 이지수 on 2022/06/29.
+//
+
+import UIKit
+
+import SnapKit
+
+final class HomeViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+}
