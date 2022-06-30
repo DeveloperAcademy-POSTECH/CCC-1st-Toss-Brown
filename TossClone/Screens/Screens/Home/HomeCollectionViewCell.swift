a18131c487371c7490d2a371ea73252443747d72
JI SOO <dlwltn815@gmail.com>
2022-06-30 22:43:03 +0900

    Feat: Stackview로 image, labele 설정 (#5)

--- TossClone/Screens/Screens/Home/HomeCollectionViewCell.swift
+++ TossClone/Screens/Screens/Home/HomeCollectionViewCell.swift
@@ -7,6 +7,30 @@
 
 import UIKit
 
-final class HomeCollectionViewCell: UICollectionViewCell {
+import SnapKit
 
+final class HomeCollectionViewCell: UICollectionViewCell {
+    
+    private let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.alignment = .center
+        stackView.axis = .vertical
+        stackView.spacing = 16
+        return stackView
+    }()
+    
+    // MARK: - func
+    
+    override func preferredLayoutAttributesFitting(_ layoutAttributes: UICollectionViewLayoutAttributes) -> UICollectionViewLayoutAttributes {
+        super.preferredLayoutAttributesFitting(layoutAttributes)
+        layoutIfNeeded()
+        
+        let size = contentView.systemLayoutSizeFitting(layoutAttributes.size)
+        var frame = layoutAttributes.frame
+        
+        frame.size.height = ceil(size.height)
+        layoutAttributes.frame = frame
+        
+        return layoutAttributes
+    }
 }
