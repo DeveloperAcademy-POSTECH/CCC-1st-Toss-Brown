a2a79f337ac566b251f853b44ebc7def03b3d769
JI SOO <dlwltn815@gmail.com>
2022-06-30 21:40:26 +0900

    Feat: HomeCollectionView 생성 (#5)

---
+++ TossClone/Screens/Screens/Home/HomeCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  HomeCollectionViewCell.swift
+//  TossClone
+//
+//  Created by 이지수 on 2022/06/30.
+//
+
+import UIKit
+
+final class HomeCollectionViewCell: UICollectionViewCell {
+
+}
--- TossClone/Screens/Screens/Home/HomeViewController.swift
+++ TossClone/Screens/Screens/Home/HomeViewController.swift
@@ -10,8 +10,31 @@ import UIKit
 import SnapKit
 
 final class HomeViewController: UIViewController {
+    private lazy var collectionview: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(HomeCollectionViewCell.self, forCellWithReuseIdentifier: "homeCell")
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .green
+    }
+}
+
+extension HomeViewController: UICollectionViewDelegateFlowLayout {
+    
+}
+
+extension HomeViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "homeCell", for: indexPath) as? HomeCollectionViewCell
+        return cell ?? UICollectionViewCell()
     }
 }
