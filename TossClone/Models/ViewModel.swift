0f233e40ba251abff767c9226971107b2e870986
JI SOO <dlwltn815@gmail.com>
2022-07-09 19:56:52 +0900

    Add: cell type에 이름과 이미지 데이터 추가

--- TossClone/Models/ViewModel.swift
+++ TossClone/Models/ViewModel.swift
@@ -17,6 +17,26 @@ enum HomeCellType {
     case tossBank
     case assets
     case consumption
+    
+    var cellName: String {
+        switch self {
+        case .tossBank:
+            return "토스뱅크"
+        case .assets:
+            return "자산"
+        case .consumption:
+            return "소비"
+        }
+    }
+    
+    var arrowImage: UIImage {
+        switch self {
+        case .consumption:
+            return UIImage() // 얘만 화살표 없음
+        default:
+            return UIImage(systemName: "chevron.right", withConfiguration: UIImage.SymbolConfiguration(pointSize: 16.0, weight: .bold))?.withTintColor(.gray) ?? UIImage()
+        }
+    }
 }
 
 struct HomeCellList {
