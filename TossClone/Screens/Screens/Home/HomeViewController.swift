e09226c0b7823af2baaa50b3f365efae7d4e61f6
JI SOO <dlwltn815@gmail.com>
2022-07-09 20:24:46 +0900

    Fix: home에 collectionview 삭제 (#9)

--- TossClone/Screens/Screens/Home/HomeViewController.swift
+++ TossClone/Screens/Screens/Home/HomeViewController.swift
@@ -13,14 +13,6 @@ final class HomeViewController: UIViewController {
     
     // MARK: - property
     
-    private lazy var collectionview: UICollectionView = {
-        let layout = UICollectionViewFlowLayout()
-        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        collectionView.delegate = self
-        collectionView.dataSource = self
-        collectionView.register(HomeCollectionViewCell.self, forCellWithReuseIdentifier: "homeCell")
-        return collectionView
-    }()
     private lazy var logoView: UIStackView = {
         var logoImage = UIImage(named: "tossLogo")
         let logoButton = UIButton()
@@ -57,35 +49,7 @@ final class HomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(collectionview)
-        collectionview.backgroundColor = .backgroundColor
-        collectionview.snp.makeConstraints {
-            $0.edges.equalToSuperview()
-        }
         setupNavigationBar()
-    }
-}
-
-extension HomeViewController: UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
-        return CGSize(width: UIScreen.main.bounds.size.width, height: 1)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let width = collectionView.frame.width - 36
-        return CGSize(width: width, height: 80)
-    }
-}
-
-extension HomeViewController: UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        4
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "homeCell", for: indexPath) as? HomeCollectionViewCell
-        cell?.setup()
-        return cell ?? UICollectionViewCell()
     }
 }
 
