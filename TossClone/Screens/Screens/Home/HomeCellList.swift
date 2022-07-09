d2796ba96d835d1d9b286742787b7278339418ed
JI SOO <dlwltn815@gmail.com>
2022-07-09 21:09:34 +0900

    Feat: cell안에 들어갈 테이블뷰 생성 (#9)
    - stack으로 토스뱅크, 자산, 소비 각각의 cell을 또 합쳐준다음, 완전 큰 cell에 stackview로 또 올려야함

--- TossClone/Screens/Screens/Home/HomeCellList.swift
+++ TossClone/Screens/Screens/Home/HomeCellList.swift
@@ -7,6 +7,30 @@
 
 import UIKit
 
-class HomeCellList: UITableView {
-
+class HomeCellList: UIView {
+    var cellList: HomeCellListData
+    
+    // MARK: - properties
+    
+    private lazy var cellTableView: UITableView = {
+        let tableView = UITableView(frame: .zero)
+        tableView.rowHeight = 70
+        tableView.separatorStyle = .none
+        tableView.separatorInset = UIEdgeInsets(top: 7.0, left: 24.0, bottom: 7.0, right: 24.0)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(HomeCellListCell.self, forCellReuseIdentifier: "HomeCellListCell")
+        return tableView
+    }()
+    
+    // MARK: - init
+    
+    init(frame: CGRect, list: [HomeCellListData]) {
+        cellList = list
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
