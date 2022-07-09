8b41268167575e1274713f75b028ca7f00cf0cd1
JI SOO <dlwltn815@gmail.com>
2022-07-09 22:26:01 +0900

    Feat: 프로퍼티 layout 설정 (#9)

--- TossClone/Screens/Screens/Home/HomeCellListCell.swift
+++ TossClone/Screens/Screens/Home/HomeCellListCell.swift
@@ -52,3 +52,55 @@ class HomeCellListCell: UITableViewCell {
     }
     
 }
+
+extension HomeCellListCell {
+    func setupViews() {
+        [listImageView, subTitle, title].forEach { addSubview($0) }
+        listImageView.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(24.0)
+            $0.width.height.equalTo(40.0)
+            $0.top.equalToSuperview().inset(20.0)
+            $0.trailing.equalToSuperview().inset(20.0)
+        }
+        subTitle.snp.makeConstraints {
+            $0.leading.equalTo(listImageView.snp.trailing).offset(24.0)
+            $0.top.equalTo(listImageView.snp.top)
+            $0.trailing.equalToSuperview()
+        }
+        title.snp.makeConstraints {
+            $0.top.equalTo(subTitle.snp.bottom).offset(5.0)
+            $0.leading.equalTo(subTitle.snp.leading)
+            $0.bottom.equalTo(listImageView.snp.bottom)
+            $0.trailing.equalToSuperview()
+        }
+        
+        switch type?.cellType {
+        case .assets:
+            setupSendButton()
+        case .consumption:
+            setupHistoryButton()
+        default:
+            return
+        }
+    }
+    
+    func setupSendButton() {
+        addSubview(sendButton)
+        sendButton.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(24.0)
+            $0.centerY.equalTo(snp.centerY)
+            $0.width.equalTo(50.0)
+            $0.height.equalTo(30.0)
+        }
+    }
+    
+    func setupHistoryButton() {
+        addSubview(historyButton)
+        sendButton.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(24.0)
+            $0.centerY.equalTo(snp.centerY)
+            $0.width.equalTo(50.0)
+            $0.height.equalTo(30.0)
+        }
+    }
+}
