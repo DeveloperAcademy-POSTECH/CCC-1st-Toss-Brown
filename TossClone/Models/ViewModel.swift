9c7caadd5a119b5ca5f4e2b4c0d1d77234afe508
JI SOO <dlwltn815@gmail.com>
2022-07-09 17:04:06 +0900

    Feat: 이미지 asset 추가 및 더미데이터 model 생성 (#9)

--- TossClone/Models/ViewModel.swift
+++ TossClone/Models/ViewModel.swift
@@ -5,14 +5,36 @@
 //  Created by 이지수 on 2022/06/30.
 //
 
+// ref: https://github.com/DeveloperAcademy-POSTECH/CCC-1st-Toss-Curry/pull/1
 import UIKit
 
-struct Title: Codable {
-    var title: String
+struct TossData {
+    let cellType: HomeCellType
+    let cellList: [HomeCellList]
 }
 
-var titleData: [Title] = [
-    Title(title: "토스뱅크"),
-    Title(title: "자산"),
-    Title(title: "소비")
-]
+enum HomeCellType {
+    case tossBank
+    case assets
+    case consumption
+}
+
+struct HomeCellList {
+    let image: String
+    let subTitle: String
+    let title: String
+}
+
+// MARK: - dummy data
+let tossBankRow = HomeCellList(image: "tossbankAds", subTitle: "일 복리 효과를 누릴 수 있어요", title: "눌러서 매일 이자 받기")
+let assetsRow1 = HomeCellList(image: "tossbank", subTitle: "토스뱅크 통장", title: "150,000원")
+let assetsRow2 = HomeCellList(image: "shinhan", subTitle: "신한은행 계좌", title: "? 원")
+let assetsRow3 = HomeCellList(image: "hana", subTitle: "하나은행 계좌", title: "? 원")
+let assetsRow4 = HomeCellList(image: "toss", subTitle: "투자·토스증권 계좌", title: "100,000원")
+let consumptionRow = HomeCellList(image: "card", subTitle: "이번 달 쓴 금액", title: "30,000원")
+
+let tossCellData = TossData(cellType: .tossBank, cellList: [tossBankRow])
+let assetsCellData = TossData(cellType: .assets, cellList: [assetsRow1, assetsRow2, assetsRow3, assetsRow4])
+let consumptionCellData = TossData(cellType: .consumption, cellList: [consumptionRow])
+
+let dummyData = [tossCellData, assetsCellData, consumptionCellData]
